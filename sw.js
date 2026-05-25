314d42f314d4990bd2d03ce6123a266a470a46e5
MrgaCRO <61421552+MrgaCRO@users.noreply.github.com>
2026-05-25 22:36:48 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,40 +1,37 @@
-const cacheName = 'snake-edu-v1.0.5'; // Svaki put kad mijenjaš igru, povećaj ovaj broj
+const cacheName = 'snake-edu-v1.1.4'; // Povećano na v1.1.4 radi prisilnog čišćenja cachea
 const assets = [
-    './',
-    'index.html',
-    'manifest.json'
+  './',
+  'index.html',
+  'manifest.json'
 ];
 
-// Instalacija Service Workera i spremanje datoteka u cache
 self.addEventListener('install', e => {
-    self.skipWaiting(); // Prisili novi SW da preuzme kontrolu odmah
-    e.waitUntil(
-        caches.open(cacheName).then(cache => {
-            return cache.addAll(assets);
-        })
-    );
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
-// Aktiviranje i čišćenje SVIH starih verzija cachea
 self.addEventListener('activate', e => {
-    e.waitUntil(
-        caches.keys().then(keys => {
-            return Promise.all(
-                keys.map(key => {
-                    if (key !== cacheName) {
-                        return caches.delete(key);
-                    }
-                })
-            );
-        }).then(() => self.clients.claim()) // Preuzmi kontrolu nad svim tabovima odmah
-    );
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== cacheName) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
 });
 
-// Dohvaćanje datoteka (Offline rad)
 self.addEventListener('fetch', e => {
-    e.respondWith(
-        caches.match(e.request).then(res => {
-            return res || fetch(e.request);
-        })
-    );
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
